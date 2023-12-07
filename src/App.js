6b9b1494b44569fdd46b5e1d3d7321215872f0cf
Iman Rahgozar Abadi <imanrahgozarabadi@dhcp-128-189-209-68.ubcsecure.wireless.ubc.ca>
2023-12-06 21:38:38 -0800

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed
+          Just wanted to check the feature branch 
         </p>
         <a
           className="App-link"
